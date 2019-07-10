ceb5c62d59824d8234c6301425bb9cf653569d0c
loserDJ <2121242943@qq.com>
2019-07-10 23:04:28 +0800

    .

--- main.js
+++ main.js
@@ -13,33 +13,52 @@ setInterval(()=>{
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // 下面可以不看
 
 function getImage(n){
-    return $(`.images > img:nth-child(${x(n)})`)
-  }
-  
-  function x(n){
-    if(n>3){
-      n = n%3
-      if (n===0){
-        n =3
-      }
-    } // n = 1 2 3
-    return n
-  }
-  
-  function 初始化(){
-    n = 1
-    $(`.images > img:nth-child(${n})`).addClass('current')
-      .siblings().addClass('enter')
-  }
-  
-  function makeCurrent($node){
-    return $node.removeClass('enter leave').addClass('current')
-  }
-  function makeLeave($node){
-    return $node.removeClass('enter current').addClass('leave')
-  }
-  function makeEnter($node){
-    return $node.removeClass('leave current').addClass('enter')
+  return $(`.images > img:nth-child(${x(n)})`)
+}
+
+function x(n){
+  if(n>3){
+    n = n%3
+    if (n===0){
+      n =3
+    }
+  } // n = 1 2 3
+  return n
+}
+
+function 初始化(){
+  n = 1
+  $(`.images > img:nth-child(${n})`).addClass('current')
+    .siblings().addClass('enter')
+}
+
+function makeCurrent($node){
+  return $node.removeClass('enter leave').addClass('current')
+}
+function makeLeave($node){
+  return $node.removeClass('enter current').addClass('leave')
+}
+function makeEnter($node){
+  return $node.removeClass('leave current').addClass('enter')
+}
